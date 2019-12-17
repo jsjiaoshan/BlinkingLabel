b1072ac51bb02a0d4c4ebc9b5db1d216d79e9c1d
jiaoshan <2977530037@qq.com>
2019-12-17 11:46:00 +0800

    Initial commit

---
+++ Example/BlinkingLabel/JSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JSAppDelegate.h
+//  BlinkingLabel
+//
+//  Created by jiaoshan on 12/17/2019.
+//  Copyright (c) 2019 jiaoshan. All rights reserved.
+//
+
+@import UIKit;
+
+@interface JSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/BlinkingLabel/JSViewController.h
@@ -0,0 +1,13 @@
+//
+//  JSViewController.h
+//  BlinkingLabel
+//
+//  Created by jiaoshan on 12/17/2019.
+//  Copyright (c) 2019 jiaoshan. All rights reserved.
+//
+
+@import UIKit;
+
+@interface JSViewController : UIViewController
+
+@end
